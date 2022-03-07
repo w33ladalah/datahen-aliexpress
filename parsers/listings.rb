51513091142b9e74b4d3f195e73cbba8166ccf14
Hendro <hendrothemail@gmail.com>
2022-03-07 20:05:00 +0700

    Fix listings parser

--- parsers/listings.rb
+++ parsers/listings.rb
@@ -1,9 +1,9 @@
 nokogiri = Nokogiri.HTML(content)
-products = nokogiri.css('li.octopus-pc-item-v3')
+products = nokogiri.css('.product-container a._2f4Ho')
 
 products.each do |product|
   href = product.at_css('a')['href'].split('?').first
-  url = URI.join('https://www.amazon.com', href).to_s
+  url = URI.join('https://www.aliexpress.com', href).to_s
   display_width = rand 800..1920
   display_height = rand 728..1280
   pages << {
@@ -13,5 +13,5 @@ products.each do |product|
 			category: page['vars']['category'],
             url: url
 		}
-    }
+  }
 end
